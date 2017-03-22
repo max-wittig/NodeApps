875f3f7fcdb3c5ca3359d90d5e4029943a22aebc
Max Wittig <max.wittig95@gmail.com>
2017-03-22 21:44:02 +0100

    fix(design): better look

--- server/routes/counter.js
+++ server/routes/counter.js
@@ -4,13 +4,6 @@ let path = require("path")
 
 module.exports = function(express)
 {
-    express.get("/counter", function (req, res)
-    {
-        res.sendFile(path.resolve("html/counter/index.html"))
-    })
 
-    express.get("/counter/index.js", function (req, res)
-    {
-        res.sendFile(path.resolve("html/counter/index.js"))
-    })
+
 }
--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ let io = require("socket.io")(http)
 require("./routes/routes")(app)
 require("./counter")(io)
 app.use("/node_modules", express.static(path.resolve('node_modules')))
-
+app.use("/counter", express.static(path.resolve('html/counter')))
 
 http.listen(3000)
